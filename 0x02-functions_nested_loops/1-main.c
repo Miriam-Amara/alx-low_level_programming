de646e90a25cdb795cac60ce6b6c7a414c7a53bf
Miriam_Ikechukwu <ikechukwumiriam@gmail.com>
2023-08-15 19:07:20 -0700

    Done

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *description - prints all lowercase alphabets
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
