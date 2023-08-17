6ad15e5850af5d692ae87c717c68a8f5f4f07c28
Miriam_Ikechukwu <ikechukwumiriam@gmail.com>
2023-08-16 21:08:11 -0700

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *add - this is a function that add two integers together
+ *@p: the first integer
+ *@q: the second inter
+ *Return: sum
+ */
+
+int add(int p, int q)
+{
+int sum;
+sum = p + q;
+return (sum);
+}
