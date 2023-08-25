64e58c5015065c02317150701663762100831e8d
Miriam_Ikechukwu <ikechukwumiriam@gmail.com>
2023-08-25 08:45:08 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
- *main - Entry
- *Return - 1
+ *main - Entry point
+ *Return: 1
  */
 
 int main(void)
 {
-  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-  return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+return (1);
 }
